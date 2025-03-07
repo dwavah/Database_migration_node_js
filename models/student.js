fb9a66b50141743ed8df91ccb8f8218e8a8a10e4
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-07 11:53:59 +0300

    modifying models/student.js

--- models/student.js
+++ models/student.js
@@ -1,24 +1,51 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Student extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class Student extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   Student.init({
+//     name: DataTypes.STRING,
+//     email: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'Student',
+//   });
+//   return Student;
+// };
+
+const { Model, DataTypes, Sequelize } = require("sequelize");
+module.exports = (Sequelize) => {
+  class Student extends Model {}
+
+  Student.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      }
+    },
+    {
+      sequelize,
+      modelName: "Student",
+      tableName: "Students",
+      timestamps: true
     }
-  }
-  Student.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Student',
-  });
+
+  );
   return Student;
-};
+}
