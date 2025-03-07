d9e885a61e512fb64e80dbfe88564483bc365965
Daniel Wavamunno <danl.wavah@gmail.com>
2025-03-07 14:33:20 +0300

    creating migration for the teachers table

---
+++ migrations/20250307113012-create-teachers.js
@@ -0,0 +1,60 @@
+// 'use strict';
+
+// /** @type {import('sequelize-cli').Migration} */
+// module.exports = {
+//   async up (queryInterface, Sequelize) {
+//     /**
+//      * Add altering commands here.
+//      *
+//      * Example:
+//      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+//      */
+//   },
+
+//   async down (queryInterface, Sequelize) {
+//     /**
+//      * Add reverting commands here.
+//      *
+//      * Example:
+//      * await queryInterface.dropTable('users');
+//      */
+//   }
+// };
+
+'use strict';
+
+const { Sequelize } = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Teachers", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      subject: {
+        type: Sequelize.STRING,
+        aloowNull: false,
+        unique: true
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        aloowNull: false
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Teachers");
+  }
+
+};
